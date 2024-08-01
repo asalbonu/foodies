167fb10143437fe071ee712d3e3a34808bce704c
asalbonu <rakhmatjonovoy@gmail.com>
2024-08-01 13:51:54 +0500

    Update config.php

--- config.php
+++ config.php
@@ -5,6 +5,6 @@ $password = '';
 $dbname = 'users1';
 $conn = new mysqli($host, $username, $password, $dbname);
 if ($conn->connect_error) { 
-    die('Ошибка подключениякБД:'. $conn->connect_error);
+    die('Ошибка подключенияк'. $conn->connect_error);
 }
 ?>
